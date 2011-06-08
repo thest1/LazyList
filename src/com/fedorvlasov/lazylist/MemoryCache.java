e9ca485cab13bd906aa4864abe837e2f01f9d43e
Fedor Vlasov <thest2@gmail.com>
2011-06-08 21:58:59 +0600

    Use SoftReference in MemoryCache to prevent OutOfMemory

--- src/com/fedorvlasov/lazylist/MemoryCache.java
+++ src/com/fedorvlasov/lazylist/MemoryCache.java
@@ -1,23 +1,24 @@
 package com.fedorvlasov.lazylist;
 
+import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import android.graphics.Bitmap;
 
 public class MemoryCache {
-    private HashMap<String, Bitmap> cache=new HashMap<String, Bitmap>();
+    private HashMap<String, SoftReference<Bitmap>> cache=new HashMap<String, SoftReference<Bitmap>>();
     
     public Bitmap get(String id){
         if(!cache.containsKey(id))
             return null;
-        return cache.get(id);
+        SoftReference<Bitmap> ref=cache.get(id);
+        return ref.get();
     }
     
     public void put(String id, Bitmap bitmap){
-        cache.put(id, bitmap);
+        cache.put(id, new SoftReference<Bitmap>(bitmap));
     }
 
     public void clear() {
         cache.clear();
     }
-
 }
