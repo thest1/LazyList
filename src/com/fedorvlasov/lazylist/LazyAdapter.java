44f0e21848c5694efe559a927654527b8a7e35b1
Fedor Vlasov <thest2@gmail.com>
2011-06-12 22:00:41 +0600

    Don't use ViewHolder to make adapter more clear

--- src/com/fedorvlasov/lazylist/LazyAdapter.java
+++ src/com/fedorvlasov/lazylist/LazyAdapter.java
@@ -35,26 +35,15 @@ public class LazyAdapter extends BaseAdapter {
         return position;
     }
     
-    public static class ViewHolder{
-        public TextView text;
-        public ImageView image;
-    }
-
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi=convertView;
-        ViewHolder holder;
-        if(convertView==null){
+        if(convertView==null)
             vi = inflater.inflate(R.layout.item, null);
-            holder=new ViewHolder();
-            holder.text=(TextView)vi.findViewById(R.id.text);;
-            holder.image=(ImageView)vi.findViewById(R.id.image);
-            vi.setTag(holder);
-        }
-        else
-            holder=(ViewHolder)vi.getTag();
-        
-        holder.text.setText("item "+position);
-        imageLoader.DisplayImage(data[position], activity, holder.image);
+
+        TextView text=(TextView)vi.findViewById(R.id.text);;
+        ImageView image=(ImageView)vi.findViewById(R.id.image);
+        text.setText("item "+position);
+        imageLoader.DisplayImage(data[position], activity, image);
         return vi;
     }
 }
