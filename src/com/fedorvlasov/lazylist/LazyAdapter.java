2d84bca288e94cce6f8edc27b7ccd14e65a83d11
johnnycube <johannes@kueber.eu>
2012-04-19 05:59:56 -0700

    Add the ability to choose cache folder initialization

--- src/com/fedorvlasov/lazylist/LazyAdapter.java
+++ src/com/fedorvlasov/lazylist/LazyAdapter.java
@@ -10,40 +10,43 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LazyAdapter extends BaseAdapter {
-    
-    private Activity activity;
-    private String[] data;
-    private static LayoutInflater inflater=null;
-    public ImageLoader imageLoader; 
-    
-    public LazyAdapter(Activity a, String[] d) {
-        activity = a;
-        data=d;
-        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        imageLoader=new ImageLoader(activity.getApplicationContext());
-    }
 
-    public int getCount() {
-        return data.length;
-    }
+	private Activity activity;
+	private String[] data;
+	private static LayoutInflater inflater = null;
+	public ImageLoader imageLoader;
 
-    public Object getItem(int position) {
-        return position;
-    }
+	public LazyAdapter(Activity a, String[] d) {
+		activity = a;
+		data = d;
+		inflater = (LayoutInflater) activity
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		imageLoader = new ImageLoader(activity.getApplicationContext(),
+				"LazyList");
+	}
 
-    public long getItemId(int position) {
-        return position;
-    }
-    
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View vi=convertView;
-        if(convertView==null)
-            vi = inflater.inflate(R.layout.item, null);
+	public int getCount() {
+		return data.length;
+	}
 
-        TextView text=(TextView)vi.findViewById(R.id.text);;
-        ImageView image=(ImageView)vi.findViewById(R.id.image);
-        text.setText("item "+position);
-        imageLoader.DisplayImage(data[position], image);
-        return vi;
-    }
+	public Object getItem(int position) {
+		return position;
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View vi = convertView;
+		if (convertView == null)
+			vi = inflater.inflate(R.layout.item, null);
+
+		TextView text = (TextView) vi.findViewById(R.id.text);
+		;
+		ImageView image = (ImageView) vi.findViewById(R.id.image);
+		text.setText("item " + position);
+		imageLoader.DisplayImage(data[position], image);
+		return vi;
+	}
 }
